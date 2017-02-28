ef07172b83697f89cb1685f3f6c54ab7ddfcffff
aponysos <aponysos@163.com>
2017-02-28 17:45:58 +0800

    insert: not completed

--- DotNet/SkipList/Program.cs
+++ DotNet/SkipList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SkipList
 {
@@ -41,30 +42,113 @@ namespace SkipList
                 this.down = down;
                 this.next = next;
             }
+            public Node(int value) : this(value, null, null)
+            {
+            }
         }
         private Node head_;
         private Random rand_ = new Random();
+        private const double PROB = 0.25;
+        private int GetRandomLevel()
+        {
+            int randomLevel = 0;
+            while (rand_.NextDouble() < PROB)
+                ++randomLevel;
+            return randomLevel;
+        }
         public SkipList()
         {
         }
         public void Init()
         {
-            head_ = new Node(0, null, null);
+            head_ = new Node(0);
         }
         public void Clear()
         {
             head_ = null;
         }
-        public bool Search(int value)
+        private Node InsertLevelNode(Node curNode, int value)
         {
-            return false;
+            Node newNode = new Node(value, null, curNode.next);
+            curNode.next = newNode;
+            return newNode;
         }
         public bool Insert(int value)
         {
+            if (head_.next == null) // empty skiplist
+            {
+                head_.next = new Node(value, null, null);
+                return true;
+            }
+
+            if (Search(value)) // replicated value
+                return false;
+
+            int curLevel = head_.value;
+            int randomLevel = GetRandomLevel();
+            Stack<Node> s = new Stack<Node>();
+            while (curLevel < randomLevel)
+            {
+                Node newNode = InsertLevelNode(head_, value);
+                newNode.down = s.Peek();
+                s.Push(newNode);
+                ++curLevel;
+            }
+
+            Node current = head_;
+            do
+            {
+                while (current.next != null && value > current.next.value)
+                    current = current.next;
+
+                Node newNode = s.Pop();
+                newNode.next = current.next;
+                current.next = newNode;
+                current = current.down; // move to next level
+                --curLevel;
+            }
+            while (curLevel >= 0);
+
             return false;
         }
         public bool Remove(int value)
         {
+            if (head_.next == null)
+                return false;
+
+            Node current = head_;
+            do
+            {
+                while (current.next != null && value > current.next.value)
+                    current = current.next;
+
+                if (current.next != null && value == current.next.value)
+                    current.next = current.next.next;
+
+                current = current.down;
+            }
+            while (current != null);
+
+            return true;
+        }
+        public bool Search(int value)
+        {
+            if (head_.next == null)
+                return false;
+
+            Node current = head_;
+            do
+            {
+                while (current.next != null && value > current.next.value)
+                    current = current.next;
+
+                if (current.next != null && value == current.next.value)
+                    return true;
+
+                current = current.down;
+            }
+            while (current != null);
+
             return false;
         }
         public override string ToString()
