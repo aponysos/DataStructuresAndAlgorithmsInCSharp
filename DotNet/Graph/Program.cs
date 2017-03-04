6e00a7e4fee0271e63de06b6885af69a19e5afd4
Aponysos <aponysos@163.com>
2017-03-04 20:17:22 +0800

    topological sort

--- DotNet/Graph/Program.cs
+++ DotNet/Graph/Program.cs
@@ -25,30 +25,31 @@ namespace Graph
             g.AddEdge(1, 5);
             g.AddEdge(2, 3);
             g.AddEdge(2, 4);
-            g.TopSort();
+            g.TopologicalSort();
         }
     }
 
+    public class Vertex
+    {
+        public string label_;
+        public Vertex(string label)
+        {
+            this.label_ = label;
+        }
+        public override string ToString()
+        {
+            return label_;
+        }
+    }
     public class Graph
     {
-        public class Vertex
+        public int maxVertices_;
+        public int numVertices_;
+        public Vertex[] vertices_;
+        public int[,] adjMatrix_;
+        public Graph(int maxVertices = 20)
         {
-            public string label_;
-            public Vertex(string label)
-            {
-                this.label_ = label;
-            }
-            public override string ToString()
-            {
-                return label_;
-            }
-        }
-        private Vertex[] vertices_;
-        private int[,] adjMatrix_;
-        private int maxVertices_;
-        private int numVertices_;
-        public Graph(int maxVertices_ = 20)
-        {
+            maxVertices_ = maxVertices;
             vertices_ = new Vertex[maxVertices_];
             adjMatrix_ = new int[maxVertices_, maxVertices_];
             numVertices_ = 0;
@@ -76,9 +77,56 @@ namespace Graph
             else
                 return false;
         }
-        public void TopSort()
+        public void TopologicalSort()
         {
-            
+            TopologicalSorter sorter = new TopologicalSorter(this);
+            sorter.Sort();
+        }
+    }
+    public class TopologicalSorter
+    {
+        private Graph g_;
+        private int nv_; // # of vertices
+        private bool[] visited_;
+        private Stack<int> s_;
+        public TopologicalSorter(Graph g)
+        {
+            g_ = g;
+            nv_ = g.numVertices_;
+            visited_ = new bool[nv_];
+            for (int i = 0; i < nv_; ++i)
+                visited_[i] = false;
+            s_ = new Stack<int>(nv_);
+        }
+        public void Sort()
+        {
+            while (s_.Count < nv_)
+                for (int i = 0; i < nv_ && !visited_[i]; ++i)
+                {
+                    if (!HasOutgoingEdge(i))
+                        VisitVertex(i);
+                }
+
+            Output();
+        }
+        private void VisitVertex(int i)
+        {
+            if (visited_[i])
+                throw new InvalidOperationException("vertex has been visited.");
+            visited_[i] = true;
+            s_.Push(i);
+        }
+        private void Output()
+        {
+            while (s_.Count > 0)
+                Console.WriteLine(g_.vertices_[s_.Pop()]);
+        }
+        private bool HasOutgoingEdge(int i)
+        {
+            for (int j = 0; j < nv_; ++j)
+                if (!visited_[j] && g_.adjMatrix_[i, j] > 0)
+                    return true;
+            return false;
         }
     }
 }
