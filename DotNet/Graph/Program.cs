bb1d441029b754a68c825106577ff44824efd814
Aponysos <aponysos@163.com>
2017-03-05 21:27:44 +0800

    test case: weighted graph

--- DotNet/Graph/Program.cs
+++ DotNet/Graph/Program.cs
@@ -103,20 +103,18 @@ namespace Graph
             g.AddVertex("E");
             g.AddVertex("F");
             g.AddVertex("G");
-            g.AddEdge(0, 1);
-            g.AddEdge(0, 2);
-            g.AddEdge(0, 3);
-            g.AddEdge(1, 2);
-            g.AddEdge(1, 3);
-            g.AddEdge(1, 4);
-            g.AddEdge(2, 3);
-            g.AddEdge(2, 5);
-            g.AddEdge(3, 5);
-            g.AddEdge(3, 4);
-            g.AddEdge(3, 6);
-            g.AddEdge(4, 5);
-            g.AddEdge(4, 6);
-            g.AddEdge(5, 6);
+            g.AddEdge(0, 1, 2);
+            g.AddEdge(0, 3, 1);
+            g.AddEdge(1, 3, 3);
+            g.AddEdge(1, 4, 10);
+            g.AddEdge(2, 0, 4);
+            g.AddEdge(2, 5, 5);
+            g.AddEdge(3, 2, 2);
+            g.AddEdge(3, 4, 2);
+            g.AddEdge(3, 5, 8);
+            g.AddEdge(3, 6, 4);
+            g.AddEdge(6, 5, 1);
+            g.AddEdge(4, 6, 6);
             g.MST();
         }
 
