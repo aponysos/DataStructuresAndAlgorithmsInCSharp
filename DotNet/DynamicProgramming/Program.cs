ca665ef4d71f633356dcdbf70bb769598386167d
aponysos <aponysos@163.com>
2017-03-13 17:34:51 +0800

    lcs: setup

--- DotNet/DynamicProgramming/Program.cs
+++ DotNet/DynamicProgramming/Program.cs
@@ -6,7 +6,32 @@ namespace DynamicProgramming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            TestLCS();
+        }
+        static void TestLCS()
+        {
+            Console.WriteLine("TestLCS:");
+            LCSAlgorithm lcs = new LCSAlgorithm("", "");
+            Console.WriteLine(lcs.LongestCommonSubstring);
+        }
+    }
+    class LCSAlgorithm
+    {
+        private string s_;
+        private string t_;
+        private string lcs_;
+        public string LongestCommonSubstring
+        {
+            get { return lcs_; }
+        }
+        public LCSAlgorithm(string s, string t)
+        {
+            s_ = s;
+            t_ = t;
+        }
+        public void Compute()
+        {
         }
     }
 }
